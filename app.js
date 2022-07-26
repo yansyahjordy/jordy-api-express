d5c31a3fb8c8c64bab3d845592feb214c0d2f2e4
yansyahjordy <yansyahjordy@gmail.com>
2022-07-27 05:10:00 +0700

    my first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv/config");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+
+//import routes
+const postsRoute = require("./routes/post");
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/post", postsRoute);
+
+//routes
+app.get("/", (req, res) => {
+  res.send("we are on home");
+});
+
+mongoose.connect(process.env.DB_CONNECTION, () => console.log("connected"));
+// console.log(process.env.DB_CONNECTION)
+
+app.listen(3000);
+``;
---
+++ routes/post.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/News");
+
+//get all post
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//get spesific post by id
+router.get("/:postID", getData, (req, res) => {
+    res.json(res.posts);
+});
+
+//delete post by id
+router.delete("/:postID", getData, async (req, res) => {
+  try {
+     await res.post.remove();
+    res.json({message:"Deleted News"});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//update post by id
+router.patch("/:postID", getData, async (req, res) => {
+ if(req.body.title != null){
+    res.post.title = req.body.title
+ }
+ if(req.body.image != null){
+    res.post.image = req.body.image
+ }
+ if (req.body.timePosted != null){
+    res.post.timePosted = req.body.timePosted
+ }
+
+ try{
+    const updatedPost = await res.post.save()
+    res.json(updatedPost)
+ }catch(err){
+    res.status(400).json({message:err.message})
+ }
+    
+});
+
+//post new data
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    image: req.body.image,
+    timePosted: req.body.timePosted,
+  });
+  try {
+    const savedPost = await post.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//get data by id
+async function getData(req, res, next) {
+  let post;
+  try {
+    post = await Post.findById(req.params.postID);
+    if (post == null) {
+      return res.status(404).json({ message: "Cant find news" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.post = post
+  next()
+}
+
+module.exports = router;
